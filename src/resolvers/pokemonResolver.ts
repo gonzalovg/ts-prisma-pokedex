51015d463ed29980864813811ead49771e14fcfd
gonzalovg <gonzaloverdugog@gmail.com>
2022-05-19 18:49:56 +0200

    mas funciones gql

--- src/resolvers/pokemonResolver.ts
+++ src/resolvers/pokemonResolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "type-graphql";
+import { Query, Resolver, Arg } from "type-graphql";
 import pokemonService from "../services/pokemonService";
 import { Pokemon } from "../typedefs/pokemon";
 
@@ -10,12 +10,33 @@ export class PokemonResolver {
         return await pokemonService.getPokemons();
     }
 
-    // @Query(() => [Pokemon])
-    // async getPokemonsByType(
-    //     @Arg('type_1') pokemonType: string,
-    // ) {
-    //     return pokemonService.getPokemonsByType(pokemonType)
-    // }
+    @Query(() => [Pokemon])
+    async getPokemonsByType(
+        @Arg('type', _type => String) pokemonType: string,
+    ) {
+        return pokemonService.getPokemonsByType(pokemonType)
+    }
+
+    @Query(() => [Pokemon])
+    async getPokemon(
+        @Arg('pokemon_name', _type => String) pokemonName: string
+    ) {
+        return pokemonService.getPokemon(pokemonName)
+    }
+    
+    @Query(() => [Pokemon])
+    async getPokemonsByGeneration(
+        @Arg('generation', _type => Number) generation: number
+    ) {
+        return pokemonService.getPokemonsByGeneration(generation)
+    }
+    
+    @Query(() => [Pokemon])
+    async deletePokemon(
+        @Arg('pokemon_name', _type => String) pokemon_name: string
+    ) {
+        pokemonService.deletePokemon(pokemon_name)
+    }
 
 
 }
